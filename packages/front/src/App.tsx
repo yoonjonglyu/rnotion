c4ec75c9d9466c86147b2f5dad074be79c1a5ebd
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-04-04 23:59:48 +0900

    feat(APP): INIT

---
+++ packages/front/src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const App: React.FC = () => {
+  return <div>app</div>;
+};
+
+export default App;
--- packages/front/src/index.tsx
+++ packages/front/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import App from './App';
 /**
  *
  * @return {string}
@@ -9,6 +11,6 @@ const rootNode = document.querySelector('#app');
 if (rootNode === null) console.error('루트 노드를 찾을 수 없습니다.');
 else {
   ReactDOM.createRoot(rootNode).render(
-    <div>새로 생긴 기능은 어떻게 구성해야할까?</div>
+    <App />
   );
 }
