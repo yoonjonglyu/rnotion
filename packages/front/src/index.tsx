311c4c2afa014c833903f6e025140e7eb83b7515
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-04-05 14:43:10 +0900

    chore: ADD ESLINT - REACT & TYPESCRIPT

--- packages/front/src/index.tsx
+++ packages/front/src/index.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-/**
- *
- * @return {string}
- */
-const rootNode = document.querySelector('#app');
 
+const rootNode = document.querySelector('#app');
 if (rootNode === null) console.error('루트 노드를 찾을 수 없습니다.');
 else {
-  ReactDOM.createRoot(rootNode).render(
-    <App />
-  );
+  ReactDOM.createRoot(rootNode).render(<App />);
 }
