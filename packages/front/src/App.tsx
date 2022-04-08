07ca0838b63433723dc0d06d53d4e763e1c19b94
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-04-08 23:28:32 +0900

    refactor: 폴더 위치 수정

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
-import PageRoutes from '../pages/PageRoutes';
+import PageRoutes from './pages/PageRoutes';
 
 const App: React.FC = () => {
   return (
