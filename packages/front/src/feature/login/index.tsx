c6bf867f645c49356905335d2283094807e62436
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-04-11 00:04:34 +0900

    feat(FEATTURE): 로그인, 메인, 워크스페이스 루트

---
+++ packages/front/src/feature/login/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginFeature: React.FC = () => {
+  return <div>login</div>;
+};
+
+export default LoginFeature;
---
+++ packages/front/src/feature/workspace/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WorkspaceFeature: React.FC = () => {
+  return <div>workspace</div>;
+};
+
+export default WorkspaceFeature;
