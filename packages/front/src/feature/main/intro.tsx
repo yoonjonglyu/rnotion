ed2c81b19da89e8a561faa9320298d3db6c5e554
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-04-24 16:13:53 +0900

    feat(MAIN): INTRO - 레이아웃 구성

--- packages/front/src/feature/main/intro.tsx
+++ packages/front/src/feature/main/intro.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import Styled from '@emotion/styled';
+
+const Container = Styled.article`
+  width: 800px;
+  margin: 0 auto;
+`;
+const LoginBox = Styled.section`
+  margin: 0 auto;
+`;
+const HeadLines = Styled.hgroup`
+  margin: 0 auto;
+`;
+
+const LoginTitle = Styled.h2`
+  text-align: center;
+`;
 
 interface IntroProps {}
 
 const Intro: React.FC<IntroProps> = () => {
-  return <div>intro</div>;
+  return (
+    <Container>
+      <HeadLines>
+        <h1>RNotion.</h1>
+        <h2>가입하신 아이디로 로그인 해주세요.</h2>
+        <h5>* 아이디가 없으시다면 회원가입을 해주세요.</h5>
+      </HeadLines>
+      <LoginBox>
+        <form>
+          <LoginTitle>Login</LoginTitle>
+          <label>
+            id:
+            <input type="text" />
+          </label>
+          <label>
+            password:
+            <input type="text" />
+          </label>
+        </form>
+        <a href="#">회원가입하기</a>
+      </LoginBox>
+    </Container>
+  );
 };
 
 export default Intro;
