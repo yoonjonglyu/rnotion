64dc0335e4311653b78b4df3b2eba4f166cdc6b1
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-01 15:28:47 +0900

    feat(PAGE): ADD SIGNUP - INIT

--- packages/front/src/pages/PageRoutes.tsx
+++ packages/front/src/pages/PageRoutes.tsx
@@ -3,10 +3,11 @@ import { useRoutes } from 'react-router-dom';
 
 import MainPage from './MainPage';
 import SigninPage from './SigninPage';
+import SignUpPage from './SignupPage';
 import WorkspacePage from './WorkspacePage';
 
 const PageRoutes: React.FC = () => {
-  return useRoutes([MainPage, SigninPage, WorkspacePage]);
+  return useRoutes([MainPage, SigninPage, SignUpPage, WorkspacePage]);
 };
 
 export default PageRoutes;
---
+++ packages/front/src/pages/SignupPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RouteObject } from 'react-router-dom';
+
+const SignUpPage: RouteObject = {
+  path: 'SignUp',
+  element: <div>SignUp</div>,
+  children: [],
+};
+
+export default SignUpPage;
