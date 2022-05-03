af29f6e152734651d0e6989afa9eb68284075063
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-03 16:18:20 +0900

    feat(FEATURE): SIGNUP - BIND REGISTER FORM

--- packages/front/src/feature/signup/index.tsx
+++ packages/front/src/feature/signup/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import RegisterForm from './registerForm';
 
 const SignupFeature: React.FC = () => {
-  return <div>signup</div>;
+  return (
+    <article>
+      <h2>signup</h2>
+      <RegisterForm />
+      <Link to="signin">로그인하기</Link>
+    </article>
+  );
 };
 
 export default SignupFeature;
