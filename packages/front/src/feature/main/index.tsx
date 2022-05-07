e0629bd9882e448c21c5b2c4c55fb43e88492a0f
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-07 17:21:58 +0900

    refactor(FEATURE): MAIN - IS AUTH 전역변수로 변경

--- packages/front/src/feature/main/index.tsx
+++ packages/front/src/feature/main/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Loading from '../../components/atoms/loading';
 import Intro from './intro';
 
+import { checkAuth } from '../../lib/hooks/auth';
+
 const MainFeature: React.FC = () => {
-  const [isAuth, setIsAuth] = useState(false);
+  const { isAuth, setIsAuth } = checkAuth();
 
   return <main>{isAuth ? <Loading /> : <Intro />}</main>;
 };
