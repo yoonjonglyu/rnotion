2b3e85d00cb4b77744b8cc550206004357fd3db5
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-09 19:08:42 +0900

    feat(FEATURE): SIGN FORM - 기본적인 이벤트처리

--- packages/front/src/feature/signin/signForm.tsx
+++ packages/front/src/feature/signin/signForm.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import Styled from '@emotion/styled';
+
+import useRouter from '../../lib/hooks/router';
 
 const Form = Styled.form`
 `;
@@ -8,16 +9,40 @@ const Form = Styled.form`
 interface SignFormProps {}
 
 const SignForm: React.FC<SignFormProps> = () => {
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const router = useRouter();
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    /**
+     * validation이 필요한 부분 사실 이런 부분은 사람이 일일이 찾아서 막기 귀찮으므로 따로 lib 같은걸 쓰는게 합리적이라 생각된다.
+     * 유의미하게 ux적인 측면에서 막는다고하면 글자수 제한, value 정합성(특수문자등) 정도 생각난다.
+     */
+    console.log(userId, userPassword);
+    router.push('/workspace');
+  };
+  const handleUserId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+  const handleUserPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserPassword(e.target.value);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <h2>로그인</h2>
       <label>
         ID:
-        <input type="text" />
+        <input type="text" value={userId} onChange={handleUserId} />
       </label>
       <label>
         PASSWORD:
-        <input type="text" />
+        <input
+          type="password"
+          value={userPassword}
+          onChange={handleUserPassword}
+        />
       </label>
       <button>로그인</button>
     </Form>
