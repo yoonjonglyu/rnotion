d4e730e5b4b24be87d3f51f95341b5a24dac7bda
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-09 19:25:06 +0900

    feat(FEATURE): SIGNUP - 회원가입 폼 기본 이벤트

--- packages/front/src/feature/signup/registerForm.tsx
+++ packages/front/src/feature/signup/registerForm.tsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import useRouter from '../../lib/hooks/router';
 
 interface RegisterFormProps {}
 
 const RegisterForm: React.FC<RegisterFormProps> = () => {
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const router = useRouter();
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(userId, userPassword);
+    router.push('/signin');
+  };
+  const handleUserId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+  const handleUserPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserPassword(e.target.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
+      <h2>회원가입</h2>
       <label>
-        이메일:
-        <input />
+        ID:
+        <input type="text" value={userId} onChange={handleUserId} />
       </label>
       <label>
-        비밀번호:
-        <input />
+        PASSWORD:
+        <input
+          type="password"
+          value={userPassword}
+          onChange={handleUserPassword}
+        />
       </label>
       <button>회원가입하기</button>
     </form>
