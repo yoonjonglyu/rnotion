f1819c431b261ba805c3e58a3cb2e387a63a0167
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-11 14:41:33 +0900

    feat(HOOK): AUTH - SIGN IN

--- packages/front/src/lib/hooks/auth.ts
+++ packages/front/src/lib/hooks/auth.ts
@@ -10,3 +10,19 @@ export const checkAuth = () => {
     setIsAuth,
   };
 };
+
+export const signin = (info: { id: string; password: string }): boolean => {
+  const tmpInfo: typeof info = {
+    id: 'user',
+    password: '1234',
+  };
+  if (info.id !== tmpInfo.id) {
+    console.error('잘못된 아이디입니다.');
+    return false;
+  }
+  if (info.password !== tmpInfo.password) {
+    console.error('잘못된 비밀번호입니다.');
+    return false;
+  }
+  return true;
+};
