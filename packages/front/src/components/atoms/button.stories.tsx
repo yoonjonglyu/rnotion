00d9092036775b4eb9c10c4a9a4b229193e04e99
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-13 17:59:35 +0900

    feat(ATOM): BUTTON - INIT

---
+++ packages/front/src/components/atoms/button.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from './button';
+export default {
+  title: '리로션/UI/Atoms/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const template: ComponentStory<typeof Button> = props => (
+  <Button {...props} />
+);
+
+export const 버튼 = template.bind({});
+버튼.args = {};
