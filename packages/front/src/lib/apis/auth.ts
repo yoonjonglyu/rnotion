a21e9010e10964fffc4bd6e2d8ea1be0041531c4
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-13 15:54:54 +0900

    feat(API): AUTH - 인터페이스

---
+++ packages/front/src/lib/apis/auth.ts
@@ -0,0 +1,32 @@
+class AuthApi {
+  apiUri: string;
+  constructor() {
+    this.apiUri = '';
+  }
+  async checkDuplicateUserId() {
+    if (true) {
+      return true;
+    }
+    return false;
+  }
+  async checkDuplicateUser() {
+    if (true) {
+      return true;
+    }
+    return false;
+  }
+  async registerUser() {
+    if (true) {
+      return true;
+    }
+    return false;
+  }
+  async signin() {
+    if (true) {
+      return true;
+    }
+    return false;
+  }
+}
+
+export default AuthApi;
