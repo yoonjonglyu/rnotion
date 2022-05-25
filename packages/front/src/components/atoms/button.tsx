188966fd8dce5a661ad3e9f56d20b43af006514e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2022-05-25 09:44:49 +0900

    feat(ATOM): BUTTON - 호버 스타일 & 디폴트 컬러

--- packages/front/src/components/atoms/button.tsx
+++ packages/front/src/components/atoms/button.tsx
@@ -3,9 +3,15 @@ import Styled from '@emotion/styled';
 
 const Btn = Styled.button`
   min-width: 40px;
-  padding: 4px;
-  border: none;
-  background: ${props => props.color || 'lightblue'};
+  padding: 6px 8px;
+  font-size: 1rem;
+  color: #fffffff2;
+  border: 0.5px solid #E8F9FD;
+  border-radius: 6px;
+  background: ${props => props.color || '#0AA1DD'};
+  &:hover {
+    border: 0.5px solid #2155CD;
+  }
 `;
 
 interface ButtonProps {
